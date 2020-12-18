f9d408e301aa1a28f0f4623486f76cbffebb8aae
Vijay Vinayaraj <vijayvinayaraj@gmail.com>
2020-12-17 22:41:32 -0800

    added animation to selection of object and clicking of backbutton

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from "./node_modules/three/build/three.module.js";
 import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "./node_modules/three/examples/jsm/loaders/GLTFLoader.js";
 
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -17,38 +18,36 @@ const loader = new GLTFLoader().setPath("3d_object/gallery2/");
 scene.background = new THREE.Color(0xffffff);
 
 var domEvents = new THREEx.DomEvents(camera, renderer.domElement);
-let new_camera={x:0,y:0,z:0}
+let new_camera = { x: 0, y: 0, z: 0 };
 let camera_x, camera_y, camera_z;
 
 loader.load("scene.gltf", function (gltf) {
-	gltf.scene.position.set(0,0,0)
- scene.add(gltf.scene);
+  gltf.scene.position.set(0, 0, 0);
+  scene.add(gltf.scene);
 });
 
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.maxDistance=1
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.maxDistance = 1;
 controls.update();
-const geometry = new THREE.BoxGeometry(100,100,100);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-//scene.add( cube );
 
-//gsap.to(cube,{duration: 20,x: 500})
 
-camera.position.set(0,0,-0.5)
 
-const gridHelper = new THREE.GridHelper( 10,10 );
-// scene.add( gridHelper );
-// scene.add( new THREE.AxisHelper(10) );
+
+const geometry = new THREE.SphereBufferGeometry(0.1);
+const material = new THREE.MeshPhongMaterial({ color: 0x00ffff,transparent: true });
+const backButton = new THREE.Mesh(geometry, material);
+
+
+camera.position.set(0, 0, -0.5);
+
 
 const animate = function () {
   requestAnimationFrame(animate);
-
   render();
 };
 
-const light =new THREE.PointLight({color:0xffffff,intensity:2})
-scene.add(light)
+const light = new THREE.PointLight({ color: 0xffffff, intensity: 2 });
+scene.add(light);
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 
@@ -59,8 +58,6 @@ function onMouseMove(event) {
   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
-
-
 window.addEventListener("resize", onWindowResize, false);
 
 function onWindowResize() {
@@ -69,11 +66,11 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   render();
 }
-const geometry1 = new THREE.PlaneBufferGeometry(1.5,1.5,1);
+const geometry1 = new THREE.PlaneBufferGeometry(1.5, 1.5, 1);
 const material1 = new THREE.MeshBasicMaterial({
   color: 0xffff00,
-  transparent:false,
-  opacity:0.5
+  transparent: false,
+  opacity: 0.5,
 });
 const plane = new THREE.Mesh(geometry1, material1);
 scene.add(plane);
@@ -82,28 +79,42 @@ plane.position.set(0, 0.3, -4.01);
 // renderer.gammaOutput = true;
 // renderer.gammaFactor = 2.2;
 
-function render() {
+
+
+function render() 
+{
+
   raycaster.setFromCamera(mouse, camera);
 
   const intersects = raycaster.intersectObjects(scene.children);
-//console.log(intersects)
+  //console.log(intersects)
   for (let i = 0; i < intersects.length; i++) {
-
-    new_camera= intersects[i].object.position;
-   
-	
-    //intersects[ i ].object.material.color.set( 0x0000ff );
+    new_camera = intersects[i].object.position;
   }
 
   renderer.render(scene, camera);
+
 }
 
 window.addEventListener("mousemove", onMouseMove, false);
 
 animate();
-
-domEvents.addEventListener(plane, "click", (event) => {
-	console.log(new_camera.x,new_camera.y,new_camera.z)
-  camera.position.set(new_camera.x,new_camera.y,new_camera.z +1.8);
-  camera.lookAt(new_camera.x,new_camera.y,new_camera.z);
+let selectCameraAnimation;
+domEvents.addEventListener(plane, "click", (event) =>
+ {
+  console.log(new_camera.x, new_camera.y, new_camera.z);
+  selectCameraAnimation=gsap.to(camera.position,{duration: 1,x:new_camera.x,y:new_camera.y,z:new_camera.z+1,onComplete:()=>{
+    plane.add(backButton)
+    backButton.position.set(-0.75,0,0.3)
+    gsap.timeline()
+    .from(backButton.material,{duration:1.5,opacity:0})
+    .to(backButton.rotation,{duration:0.1,z:0.1,repeat:-1});      
+  }})
+  camera.lookAt(new_camera.x, new_camera.y, new_camera.z);
 });
+
+domEvents.addEventListener(backButton,"click",(event)=>{
+  gsap.to(backButton.material,{duration:0.5,opacity:0})
+  selectCameraAnimation.reverse();
+  plane.remove(backButton)
+})
